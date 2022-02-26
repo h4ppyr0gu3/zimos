3f94d507a4fed77413397c3579b21ba8456f8439
David Rogers <rogersdpdr@gmail.com>
2022-02-26 14:24:09 +0100

    initial frame

---
+++ README.md
@@ -0,0 +1,7 @@
+# this repository is a snapshot of my system in case of the unthinkable
+
+the script will install and set all the defaults and my preferences
+
+# Debian
+
+David Rogers
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+# https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip font file url
+# neovim config: ./init.vim
+# wallpaper library: 
+# icon packs: Bonny-Dark-Icons
+# installed applications
