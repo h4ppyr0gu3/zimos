984625d83050062154be78a9ddd2ac88c079f99b
David Rogers <rogersdpdr@gmail.com>
2022-03-18 11:03:24 +0100

    uupdate

--- README.md
+++ README.md
@@ -1,7 +1,62 @@
 # this repository is a snapshot of my system in case of the unthinkable
 
 the script will install and set all the defaults and my preferences
+and also because i tend to break it quite often
 
 # Debian
+
+to add the created user to the sudoers file type the following command
+as root user where $user is your user name 
+
+``` bash 
+apt install sudo -y
+echo "$user ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/$user 
+```
+
+copy the packages.txt file and edit it how you would like
+
+``` bash
+wget url -o packages.txt
+```
+
+run the install script in the same directory as the package.txt file
+or pass the packages flag to the script with the directory of the packages.txt file
+
+``` bash 
+sudo wget url -o - | sh 
+```
+
+or
+
+``` bash
+bash -c "$(wget -qO - '$url')" '' parameters
+```
+
+flags and options
+
+-k/--kernel [full image name ie: linux-image-5.16.0-4-amd64 ]
+    upgrades kernel to specified version 
+    please ensure the specified version is available in default repo 
+-p/--package-dir [path/to/packages.txt]
+    installs packages specified in the file, not necessarily txt
+-d/--desktop 
+    specifies that the graphical environment will be installed
+-s/--server
+    specifes no graphical elements will be installed
+-l/--language [languages to install in list or file]
+    rust 
+    ruby
+    elixir
+    python
+    node
+    go
+-c/--custom [custom package list from available packages not in apt repo]
+    ly display manager
+    sirula launcher
+
+    
+
+
+
 
 David Rogers
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip font file url
 # neovim config: ./init.vim
@@ -6,16 +6,156 @@
 # icon packs: Bonny-Dark-Icons
 # installed applications
 
+# ANSI escape codes for printf
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
 user=$(whoami)
-packages=$(echo sudo firefox sway swayidle git build-essential bison brightnessctl nvim)
+package_dir=""
+DESKTOP=1
+SERVER=1
+KERNEL=""
+available_languages=( "ruby" "python" )
+available_custom=( "ly" )
+languages=()
+custom=()
 
-su - root
-apt update && apt upgrade
-apt install sudo -y
-echo "$user ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/$user 
-apt install $packages
+# main () {
+# }
 
+print_help () {
+    echo print_help
+}
 
-exit
+while [ "$1" != "" ]
+do
+    case "$1" in
+        -k | --kernel)
+            if [ "$2" = "" ]
+            then
+                echo -e "${RED}ERROR: No kernel given ${NC}"
+                exit
+            fi
+            KERNEL=$2
+            shift 2
+            ;;
+        -d | --desktop)
+            DESKTOP=$(echo 0)
+            shift
+            ;;  
+        -s | --server)
+            SERVER=$(0)
+            shift
+            ;;
+        -l | --language) 
+            while [[ $2 =~ ^[a-zA-Z]+$ ]] && [[ $2 != "" ]] ; do
+                if ! [[ " ${available_languages[*]} " =~ " $2 " ]] ;
+                then 
+                    echo language not available: $2
+                    echo available languages: ${available_languages[*]}
+                    exit
+                fi
+                languages+=$2
+                shift
+            done
+            shift
+            ;;
+        -p | --package-dir)
+            if [[ $2 =~ ^/ ]]; then 
+                package_dir=$2 
+                shift 2
+            else
+                echo -e "${RED}ERROR: incorrect file path, please use full path ${NC}"
+                exit
+            fi
+            ;;
+        -c | --custom) 
+            while [[ $2 =~ ^[a-zA-Z]+$ ]] && [[ $2 != "" ]] ; do
+                custom+=$2
+                shift
+            done
+            shift
+            ;;
+        -h | --help)
+            print_help
+            exit
+            ;;
+        *)
+            echo -e "${ORANGE}WARNING: Unknown argument ${NC}\"$1\""
+            shift
+            ;;
+    esac
+done
 
+verify_parameters () {
+if [ $DESKTOP -eq 1 ] && [ $SERVER -eq 1 ];
+then
+    echo please specify either desktop or server installation
+fi
+}
 
+# accept_parameters () {
+echo "######################################################"
+echo -e "#           ${GREEN}Verify installation parameters${NC}           #"
+echo "######################################################"
+
+if [ $DESKTOP -eq 0 ];
+then
+    echo Desktop installation
+fi
+
+if [ $SERVER -eq 0 ] && [ $DESKTOP -eq 1 ];
+then
+    echo Server installation
+fi
+
+if ! [ "$KERNEL" = "" ]; 
+then 
+    echo Kernel-version: $KERNEL 
+fi
+
+if ! [ "$languages" = "" ];
+then
+    echo Languages: $languages
+fi
+
+if ! [ "$custom" = "" ];
+then
+    echo Custom/git software: $custom
+fi
+
+if ! [ "$package_dir" = "" ]; 
+then 
+    echo package.txt directory: $package_dir 
+else
+    echo Using default package directory: $package_dir
+fi
+
+echo "######################################################"
+read -p "Do you want to continue with install (y/n): " response
+
+if [[ $response = y* ]] || [[ $response = Y* ]] ;
+then
+    echo Starting installation
+else
+    echo Not installing
+    exit
+fi
+
+# install () {
+# sudo apt update && sudo apt upgrade
+#
+# sudo apt install $kernel_version
+# sudo apt install < $packages -y
+
+# main "$@"
