c08a1c7b81d90866b2214cfaad99a728456b9337
David Rogers <rogersdpdr@gmail.com>
2022-03-20 15:06:45 +0100

    added ruby with rbenv optionally

--- install.sh
+++ install.sh
@@ -230,20 +230,23 @@ ly_install () {
 }
 
 ruby_install () {
-    debug "rbenv install: cloning repo"
-    git clone https://github.com/rbenv/rbenv.git /home/$user/.rbenv
-    debug "making rbenv"
-    cd /home/$user/.rbenv && src/configure && make -C src
-    debug "adding to path"
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/$user/.bashrc
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/$user/.zshrc
-    debug "rbenv init"
-    /home/$user/.rbenv/bin/rbenv init
-    mkdir /home/$user/.rbenv/plugins
-    cd /home/$user/.rbenv/plugins
-    debug "cloning rbenv ruby-build"
-    git clone https://github.com/rbenv/ruby-build.git 
-    cd
+    if [[ " ruby " =~ " ${languages[*]} " ]] ;
+    then
+        debug "rbenv install: cloning repo"
+        git clone https://github.com/rbenv/rbenv.git /home/$user/.rbenv
+        debug "making rbenv"
+        cd /home/$user/.rbenv && src/configure && make -C src
+        debug "adding to path"
+        echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/$user/.bashrc
+        echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/$user/.zshrc
+        debug "rbenv init"
+        /home/$user/.rbenv/bin/rbenv init
+        mkdir /home/$user/.rbenv/plugins
+        cd /home/$user/.rbenv/plugins
+        debug "cloning rbenv ruby-build"
+        git clone https://github.com/rbenv/ruby-build.git 
+        cd
+    fi
 }
 
 main "$@"
