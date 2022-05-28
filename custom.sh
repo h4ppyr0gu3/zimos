7452fc91580e89c6211c6e54684e9595bc8da95f
David Rogers <rogersdpdr@gmail.com>
2022-05-28 18:13:49 +0200

    proto v 1

--- custom.sh
+++ custom.sh
@@ -1,14 +1,16 @@
 custom_packages () {
   mkdir -p $HOME/AUR
-  # sudo pacman -Sy fakeroot git
-  if [[ " ruby " =~ " ${custom[*]} " ]]; then ruby; fi
-  if [[ " crystal " =~ " ${custom[*]} " ]]; then crystal; fi
-  if [[ " elixir " =~ " ${custom[*]} " ]]; then elixir; fi
-  if [[ " virtualization " =~ " ${custom[*]} " ]]; then virtualization; fi
+  pac_install fakeroot git
+  if [[ " asdf " =~ " ${custom[*]} " ]]; then asdf; fi
+  if [[ " ly " =~ " ${custom[*]} " ]]; then ly; fi
+  if [[ " slack " =~ " ${custom[*]} " ]]; then slack; fi
+  if [[ " sway " =~ " ${custom[*]} " ]]; then sway; fi
+  if [[ " zsh " =~ " ${custom[*]} " ]]; then zsh; fi
 }
 
 aur_clone () {
   cd $HOME/AUR
+  pac_install git
   files_before=($(ls -f))
   git clone $1
   files_after=($(ls -f))
@@ -18,8 +20,10 @@ aur_clone () {
   cd $HOME
 }
 
-ruby () {
-  aur_clone https://aur.archlinux.org/rbenv.git
+asdf () {
+  aur_clone https://aur.archlinux.org/asdf-vm.git
+  echo ". /opt/asdf-vm/asdf.sh" >> $HOME/.zshrc
+  echo ". /opt/asdf-vm/asdf.sh" >> $HOME/.bashrc
 }
 
 ly () {
@@ -28,34 +32,36 @@ ly () {
   sudo systemctl disable getty@tty2.service
 }
 
-crystal () {
-  echo crystal
-}
-
 slack () {
   aur_clone https://aur.archlinux.org/slack-desktop.git
 }
 
-elixir () {
-  echo elixir
-}
-
 virtualization () {
-  echo elixir
-}
-
-screenshots () {
-  mkdir $HOME/Screenshots
-  sudo pacman -Sy grim slurp
+  echo virtualization env
 }
 
 sway () {
-  echo sway
+  pac_install sway grim slurp neovim ranger
+  git clone https://github.com/h4ppyr0gu3/dotfiles.git
+  aur_clone https://aur.archlinux.org/libinput-gestures.git
+  mv -rf ./dotfiles/sway/ $HOME/.config/sway
+  mv -rf ./dotfiles/scripts/ $HOME/.config/scripts
+  mv -rf ./dotfiles/qutebrowser/ $HOME/.config/qutebrowser
+  mv -rf ./dotfiles/alacritty/ $HOME/.config/alacritty
+  mv -rf ./dotfiles/nvim/ $HOME/.config/nvim
+  mv ./dotfiles/.libinput-gestures.conf $HOME/.config/.libinput-gestures.conf
+  mv -rf ./dotfiles/mako/ $HOME/.config/mako
+  mv -rf ./dotfiles/sounds/ $HOME/.config/sounds
+  mv -rf ./dotfiles/ranger/ $HOME/.config/ranger
+  mv -rf ./dotfiles/wallpapers/ $HOME/.config/wallpapers
+  rm -rf dotfiles
+  mkdir -p $HOME/Screenshots
 }
 
 zsh () {
   echo zsh
-      sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-      sudo chsh -s $(which zsh)
-      git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  sudo chsh -s $(which zsh)
+  git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  curl https://raw.githubusercontent.com/h4ppyr0gu3/dotfiles/master/.zshrc -o $HOME/.zshrc
 }
